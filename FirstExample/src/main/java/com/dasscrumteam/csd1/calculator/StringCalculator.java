3908f7c336124e4bcbe93476faf876c79dbf22ca
andreasschliep <as@dasscrumteam.com>
2025-04-26 10:57:04 +0200

    FirstExample unit tests and implementation

--- FirstExample/src/main/java/com/dasscrumteam/csd1/calculator/StringCalculator.java
+++ FirstExample/src/main/java/com/dasscrumteam/csd1/calculator/StringCalculator.java
@@ -1,9 +1,23 @@
 // src/main/java/com/dasscrumteam/csd1/calculator/StringCalculator.java
 package com.dasscrumteam.csd1.calculator;
 
+import java.util.Arrays;
+
+
 public class StringCalculator {
-    public int add(String numbers) {
+    public Double calc(String numbers) {
         // TDD exercise: Implement string calculator kata
-        return 0;
+        try {
+            // parse String to find + and add the numbers
+            String[] numberArray = numbers.split("\\+");
+            // iterate through array
+            Double sum = 0.0;
+            for (int i = 0; i < numberArray.length; i++) {
+                sum += Double.parseDouble(numberArray[i]);
+            }
+            return sum;
+        } catch (NumberFormatException e) {
+            return 0.0;
+        }
     }
 }
--- FirstExample/src/test/java/com/dasscrumteam/csd1/calculator/StringCalculatorTest.java
+++ FirstExample/src/test/java/com/dasscrumteam/csd1/calculator/StringCalculatorTest.java
@@ -1,15 +1,45 @@
 // test/java/com/dasscrumteam/csd1/calculator/StringCalculatorTest.java
 package com.dasscrumteam.csd1.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
+    private StringCalculator calculator;
+
+    // Add setup
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
     @Test
     void shouldReturnZeroForEmptyString() {
-        StringCalculator calculator = new StringCalculator();
-        assertEquals(0, calculator.add(""));
+
+        assertEquals(0, calculator.calc(""));
     }
+
+    @Test
+    void shouldReturnOneForOne(){
+        assertEquals(1, calculator.calc("1"));
+    }
+
+    @Test
+    void shouldReturnTwoForOnePlusOne(){
+        assertEquals(2, calculator.calc("1+1"));
+    }
+
+    @Test
+    void shouldReturnZeroForInvalidCalculation(){
+        assertEquals(0, calculator.calc("A+B+C"));
+    }
+
+    @Test
+    void shouldReturnThreeForOneAndHalfPlusOneAndHalf() {
+        assertEquals(3, calculator.calc("1.5+1.5"));
+    }
+
 
 }
 
