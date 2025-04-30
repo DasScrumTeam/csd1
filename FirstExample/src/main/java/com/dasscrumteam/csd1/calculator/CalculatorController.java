dfb8db108743f82f95acf7f6ead03724cbe7d63c
andreasschliep <as@dasscrumteam.com>
2025-04-30 23:47:58 +0200

    Gherkin Acceptance Test Refactoring with MVC pattern

--- FirstExample/src/main/java/com/dasscrumteam/csd1/calculator/CalculatorController.java
+++ FirstExample/src/main/java/com/dasscrumteam/csd1/calculator/CalculatorController.java
@@ -11,7 +11,7 @@ public class CalculatorController {
 
     public void run() {
         String input = view.getInput();
-6       Double result = calculator.calc(input);
+        Double result = calculator.calc(input);
         view.showResult(result);
     }
 }
